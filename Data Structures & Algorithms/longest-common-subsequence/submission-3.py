496ae1a7322a091957f60d27a1acb240493b5089
Rasmin <rasmin1738@gmail.com>
2026-07-20 20:30:45 -0400

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # Let LCA(i, j) denote the longest common subsequence between
+        # the first i characters of text1 and the first j characters of 
+        # word2
+
+        # 0 <= i <= n
+        # 0 <= j <= m
+
+        # Solve LCS(n, m)
+
+        n = len(text1)
+        m = len(text2)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        return dp[n][m]
