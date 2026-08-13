e5a48845483f6a7c2e72a09822e002ad8d00c8d4
Rasmin <rasmin1738@gmail.com>
2026-08-13 16:39:54 -0400

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = {}
+
+        for src, dst in edges:
+            if src not in adj:
+                adj[src] = []
+            if dst not in adj:
+                adj[dst] = []
+            adj[src].append(dst)
+            adj[dst].append(src)
+        
+        visited = set()
+        
+        def bfs(node):
+            q = deque()
+
+            q.append((node, None))
+            visited.add(node)
+
+            while q:
+                cur, parent = q.popleft()
+
+                for neighbor in adj.get(cur, []):
+                    if neighbor == parent:
+                        continue
+                    if neighbor in visited and neighbor != parent:
+                        return False
+                    
+                    q.append((neighbor, cur))
+                    visited.add(neighbor)
+            
+            return True
+        
+        res = bfs(0)
+
+        return res and len(visited) == n
+                
